b351f2a4e014297b7b19532ebe6332ff7f0845c6
Michael Yagudaev <michaelyagudaev@gmail.com>
2020-06-14 02:11:28 -0700

    get things to work

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,19 +1,29 @@
 class Setting < ApplicationRecord
   before_save :run_program
 
-  PATH_TO_DEMOS = '~/Documents/rpi-rgb-led-matrix/examples-api-use'.freeze
+  PATH_TO_DEMOS = '/home/pi/Documents/rpi-rgb-led-matrix/examples-api-use'.freeze
 
   def run_program
     kill_previous if pid
-    o, e, s = Open3.capture3 `sudo #{PATH_TO_DEMOS}/demo -D1 --led-rows=64 --led-cols=64 --led-slowdown-gpio=1 --led-scan-mode=0 --led-pixel-mapper="Rotate:90" --led-brightness=10 --led-daemon #{PATH_TO_DEMOS}/pictures/strawberry.ppm -m 0`
-    puts o
-    puts e
+    s = run "sudo #{PATH_TO_DEMOS}/demo -D1 --led-rows=64 --led-cols=64 --led-slowdown-gpio=1 --led-scan-mode=0 --led-pixel-mapper=\"Rotate:90\" --led-brightness=10 --led-daemon #{PATH_TO_DEMOS}/pictures/strawberry.ppm -m 0"
 
     self.pid = s.pid
   end
 
   def kill_previous
-    output = `sudo kill -9 #{pid}`
-    puts output
+    run "sudo kill -9 #{pid}"
+  end
+
+  def run(cmd)
+    if ENV['SIMULATE']
+      o, e, s = Open3.capture3 "echo \"#{cmd}\""
+    else
+      o, e, s = Open3.capture3 cmd
+    end
+
+    puts o if o.present?
+    puts e if e.present?
+
+    s
   end
 end
