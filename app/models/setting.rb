b1a2c97b9abdbd881055001c496e3ea34210f5de
Michael Yagudaev <michaelyagudaev@gmail.com>
2020-06-13 20:52:22 -0700

    change where the if is located

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -4,7 +4,7 @@ class Setting < ApplicationRecord
   PATH_TO_DEMOS = '~/Documents/rpi-rgb-led-matrix/examples-api-use'.freeze
 
   def run_program
-    kill_previous
+    kill_previous if pid
     o, e, s = Open3.capture3 `sudo #{PATH_TO_DEMOS}/demo -D1 --led-rows=64 --led-cols=64 --led-slowdown-gpio=1 --led-scan-mode=0 --led-pixel-mapper="Rotate:90" --led-brightness=10 --led-daemon #{PATH_TO_DEMOS}/pictures/strawberry.ppm -m 0`
     puts o
     puts e
@@ -13,7 +13,7 @@ class Setting < ApplicationRecord
   end
 
   def kill_previous
-    output = `sudo kill -9 #{pid}` if pid
+    output = `sudo kill -9 #{pid}`
     puts output
   end
 end
