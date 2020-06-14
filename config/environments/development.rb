8461dfac3d5c3845e4943205cc9c6e9e0ddd0ce0
Michael Yagudaev <michaelyagudaev@gmail.com>
2020-06-14 02:51:44 -0700

    temporary turn off csrf protection

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
@@ -55,5 +54,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_controller.forgery_protection_origin_check = false
+  config.action_controller.allow_forgery_protection = false
 end
